1d7dab1072a05e54129168face762f08cdb4f50d
owenmccomas <owen@mccomas.me>
2024-06-15 11:57:08 -0700

    Update NODE_ENV in env.js

--- src/env.js
+++ src/env.js
@@ -8,11 +8,11 @@ export const env = createEnv({
    */
   server: {
     // DATABASE_URL: z.string().url(),
-    // NODE_ENV: z
-      // .enum(["development", "test", "production"])
-      // .default("development"),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
     // NEXTAUTH_SECRET:
-    //   process.env.NODE_ENV === "production"
+      // process.env.NODE_ENV === "production"
     //     ? z.string()
     //     : z.string().optional(),
     // NEXTAUTH_URL: z.preprocess(
@@ -41,7 +41,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     // DATABASE_URL: process.env.DATABASE_URL,
-    // NODE_ENV: process.env.NODE_ENV,
+    NODE_ENV: process.env.NODE_ENV,
     // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
